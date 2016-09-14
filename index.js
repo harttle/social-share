4dbb3c0c36980ff197bef2cb39e1d31550bc8230
harttle <yangjvn@126.com>
2016-09-14 20:05:41 +0800

    migrate to new version

--- index.js
+++ index.js
@@ -3,27 +3,47 @@ var weiboConfig = {
         title: document.title,
         url: encodeURIComponent(location.href)
     },
-    links = {
+    linkObj = {
         facebook: 'https://www.facebook.com/harttle',
-        qrcode: {
-            url: location.href
-        },
-        'google-plus': {
-            index: 2,
-            url: 'https://plus.google.com/+杨珺'
-        },
-        weibo: {
-            index: 1,
-            url: 'http://v.t.sina.com.cn/share/share.php?' + $.param(weiboConfig)
-        },
+        qrcode: location.href,
+        'google-plus': 'https://plus.google.com/+杨珺',
+        weibo: 'http://v.t.sina.com.cn/share/share.php?' + $.param(weiboConfig),
         wechat: location.href,
         linkedin: 'https://linkedin.com/in/harttle',
         rss: 'http://harttle.com/feed.xml',
         github: 'https://github.com/harttle',
         twitter: 'https://twitter.com/harttleharttle'
-    };
+    },
+    linkArr = [{
+        style: 'facebook',
+        url: 'https://www.facebook.com/harttle'
+    }, {
+        style: 'qrcode',
+        url: location.href
+    }, {
+        style: 'google-plus',
+        url: 'https://plus.google.com/+杨珺'
+    }, {
+        style: 'weibo',
+        url: 'http://v.t.sina.com.cn/share/share.php?' + $.param(weiboConfig)
+    }, {
+        style: 'wechat',
+        url: location.href
+    }, {
+        style: 'linkedin',
+        url: 'https://linkedin.com/in/harttle'
+    }, {
+        style: 'rss',
+        url: 'http://harttle.com/feed.xml'
+    }, {
+        style: 'github',
+        url: 'https://github.com/harttle'
+    }, {
+        style: 'twitter',
+        url: 'https://twitter.com/harttleharttle}'
+    }];
 
-$('section.lg').socialShare({ links: links, size: 'lg'});
-$('section.md').socialShare({ links: links, size: 'md'});
-$('section.sm').socialShare({ links: links, size: 'sm'});
-$('section.xs').socialShare({ links: links, size: 'xs'});
+$('section.lg').socialShare(linkObj, { size: 'lg' });
+$('section.md').socialShare(linkObj);
+$('section.sm').socialShare(linkArr, { size: 'sm' });
+$('section.xs').socialShare(linkArr, { size: 'xs', blank: false });
